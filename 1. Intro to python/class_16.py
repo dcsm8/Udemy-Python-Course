3bf97375940e88ed75196018ea3055e6c89b44b8
David <david@Davids-MacBook-Pro.local>
2019-01-01 18:19:56 -0500

    Add Class 16 - Dictionaries

---
+++ 1. Intro to python/class_16.py
@@ -0,0 +1,31 @@
+# Python dictionaries
+
+my_friends = {
+    'Jose': 6,
+    'Rolf': 12,
+    'Anne': 6
+}
+
+my_friends = {
+    'Jose': {'last_seen': 6},
+    'Rolf': {'surname': 'Smith'},
+    'Anne': 6
+}
+
+players = [
+    {
+        'name': 'Rolf',
+        'numbers': (13, 22, 3, 6, 9)
+    },
+    {
+        'name': 'John',
+        'numbers': (22, 3, 5, 7, 9)
+    }
+]
+
+player_one = players[0]
+
+print(player_one['name'])
+print(player_one['numbers'])
+
+print(sum(player_one['numbers']))
