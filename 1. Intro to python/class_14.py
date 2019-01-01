48f3e93f2e84b163b5223348e003a02e4bea3d5b
David <david@Davids-MacBook-Pro.local>
2019-01-01 17:54:24 -0500

    Add class 14

---
+++ 1. Intro to python/class_14.py
@@ -0,0 +1,10 @@
+# Advanced set oprations
+
+set_one = {1, 2, 3, 4, 5}
+set_two = {1, 3, 5, 7, 9, 11}
+
+print(set_one.intersection(set_two))
+
+print(set_one.union(set_two))
+
+print(set_one.difference(set_two))
