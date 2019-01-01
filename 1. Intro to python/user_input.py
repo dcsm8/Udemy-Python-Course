395acb4a13196f3e1257a1ce0ab563944e020d5e
David <david@Davids-MacBook-Pro.local>
2019-01-01 17:46:26 -0500

    User Input Exercise

---
+++ 1. Intro to python/user_input.py
@@ -0,0 +1,11 @@
+# First, ask the user for their name. Then, print out the greeting "Hello, NAME"
+# Remember the uppercase H, the comma, and the space between words!
+
+name = input("What is your Name? ")
+print(f'Hello, {name}')
+
+# Secondly, ask the user for their age and convert it into a number.
+# Then, print out how many months that equals to (you'll have to multiply the age by 12).
+
+age = input("What is your Age? ")
+print(int(age) * 12)
