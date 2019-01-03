a7b3f8bf347499d35cc96ec8e49aa1cc1d3d39d7
dcsm8 <43980173+dcsm8@users.noreply.github.com>
2019-01-03 17:13:11 -0500

    Add class_58 - classmethod and staticmethodd

---
+++ 4. Object-Oriented Programming/class_58.py
@@ -0,0 +1,39 @@
+# @classmethod and @staticmethod
+
+
+class Student:
+    def __init__(self, name, school):
+        self.name = name
+        self.school = school
+        self.marks = []
+
+    def average(self):
+        return sum(self.marks) / len(self.marks)
+
+
+rolf = Student('Rolf', 'MIT')
+
+rolf.marks.append(78)
+rolf.marks.append(99)
+
+print(rolf.average())
+
+
+class Foo:
+    @classmethod
+    def hi(cls):
+        print(cls.__name__)
+
+
+my_object = Foo()
+my_object.hi()
+
+
+class Bar:
+    @staticmethod
+    def hi():
+        print('Hello, I don\'t take any arguments.')
+
+
+another_object = Bar()
+another_object.hi()
