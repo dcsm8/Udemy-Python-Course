6e8800dafda41d7bacd798fb4f572909ac288a0e
dcsm8 <43980173+dcsm8@users.noreply.github.com>
2019-01-03 01:45:00 -0500

    Add exercise_7

---
+++ 2. Functions and Loops in Python/exercise_7.py
@@ -0,0 +1,23 @@
+import random
+
+# This line creates a set with 6 random numbers
+lottery_numbers = set(random.sample(list(range(22)), 6))
+
+# Here are your players; find out who has the most numbers matching lottery_numbers!
+players = [
+    {'name': 'Rolf', 'numbers': {1, 3, 5, 7, 9, 11}},
+    {'name': 'Charlie', 'numbers': {2, 7, 9, 22, 10, 5}},
+    {'name': 'Anna', 'numbers': {13, 14, 15, 16, 17, 18}},
+    {'name': 'Jen', 'numbers': {19, 20, 12, 7, 3, 5}}
+]
+
+# Then, print out a line such as "Jen won 1000.".
+# The winnings are calculated with the formula:
+# 100 ** len(numbers_matched)
+
+results = {player['name']: player['numbers']
+           & lottery_numbers for player in players}
+
+winner = max(results, key=results.get)
+winnings = 100 ** len(results[winner])
+print(f'{winner} got {winnings}')
