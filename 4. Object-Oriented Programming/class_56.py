67981572e22923b84cd9e4e8ab330fd32461b36a
dcsm8 <43980173+dcsm8@users.noreply.github.com>
2019-01-03 16:40:32 -0500

    Add class_56 - Inheritance

---
+++ 4. Object-Oriented Programming/class_56.py
@@ -0,0 +1,29 @@
+# Inheritance
+
+
+class Student:
+    def __init__(self, name, school):
+        self.name = name
+        self.school = school
+        self.marks = []
+
+    def average(self):
+        return sum(self.marks) / len(self.marks)
+
+
+class WorkingStudent(Student):
+    def __init__(self, name, school, salary):
+        super().__init__(name, school)
+        self.salary = salary
+
+    def weekly_salary(self):
+        return self.salary * 37.5
+
+
+rolf = WorkingStudent('Rolf', 'MIT', 15.50)
+print(rolf.salary)
+
+rolf.marks.append(57)
+rolf.marks.append(99)
+
+print(rolf.average())
