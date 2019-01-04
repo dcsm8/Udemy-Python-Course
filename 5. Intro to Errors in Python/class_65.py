e97e659d99d549b241ae66f067865ce7c0c21b98
dcsm8 <43980173+dcsm8@users.noreply.github.com>
2019-01-04 14:18:14 -0500

    Add class_65 - Errors examples

---
+++ 5. Intro to Errors in Python/class_65.py
@@ -0,0 +1,27 @@
+class Car:
+    def __init__(self, make, model):
+        self.model = model
+        self.make = make
+
+    def __repr__(self):
+        return f'<Car {self.make} {self.model}>'
+
+
+class Garage:
+    def __init__(self):
+        self.cars = []
+
+    def __len__(self):
+        return len(self.cars)
+
+    def add_car(self, car):
+        if not isinstance(car, Car):
+            raise TypeError(
+                f'Tried to add a `{car.__class__.__name__}` to the garage, but you can only add `Car` objects')
+        self.cars.append(car)
+
+
+ford = Garage()
+
+ford.add_car('Fiesta')
+print(len(ford))
