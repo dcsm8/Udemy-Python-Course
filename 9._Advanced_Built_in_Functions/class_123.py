a00913aa0f0f1da9e5d8fcec151a1ced765b0a88
dcsm8 <43980173+dcsm8@users.noreply.github.com>
2019-01-10 21:18:48 -0500

    Add class_123 - Iterable

---
+++ 9._Advanced_Built_in_Functions/class_123.py
@@ -0,0 +1,35 @@
+# Iterable
+
+
+class FirstHundredGenerator:
+    def __init__(self):
+        self.number = 0
+
+    def __next__(self):
+        if self.number < 100:
+            current = self.number
+            self.number += 1
+            return current
+        else:
+            raise StopIteration()
+
+    def __iter__(self):
+        return self
+
+
+print(sum(FirstHundredGenerator()))
+
+
+class AnotherIterable:
+    def __init__(self):
+        self.cars = ['Fiesta', 'Focus']
+
+    def __len__(self):
+        return len(self.cars)
+
+    def __getitem__(self, i):
+        return self.cars[i]
+
+
+for car in AnotherIterable():
+    print(car)
